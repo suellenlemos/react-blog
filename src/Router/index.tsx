3fdcea7138b0a24005289184ffcfb6c0160e26b9
suellen <suellenflickr@gmail.com>
2024-07-19 12:28:03 -0300

    add application routes and its pages

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, Login, NotFound, Register } from '../view/pages';
 
-const Layout = () => {
-  return <div>Layout</div>;
-};
+import { AuthGuard } from './AuthGuard';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/register" element={<h1>Register</h1>} />
-          <Route path="/login" element={<h1>Login</h1>} />
+        <Route element={<AuthGuard isPrivate={false} />}>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Route>
 
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="*" element={<h1>Página Não Encontrada</h1>} />
+        <Route element={<AuthGuard isPrivate />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
